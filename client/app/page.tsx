5bfe41eae2687936811ad99e71639e827f7b3a7e
SaifHayek <seefhayek4@gmail.com>
2023-02-17 22:00:54 +0200

    front-end setup

---
+++ client/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Cairo } from "@next/font/google";
+import styles from "./page.module.css";
+
+const Home = () => {
+  return (
+    <h1 className="text-primary bg-light shadow-inner font-extrabold p-2">
+      مرحبا
+    </h1>
+  );
+};
+export default Home;
