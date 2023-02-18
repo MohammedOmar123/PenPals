561df4e0665808aa88801df9721b5c6298c5fe5e
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-02-18 19:59:22 +0200

    deleted packtum package and use supertest instead of it relates #19

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as pactum from 'pactum';
+import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 let app: INestApplication;
@@ -16,13 +16,13 @@ beforeAll(async () => {
 
 describe('Users', () => {
   it('should return a list of users', async () => {
-    return pactum.spec().get('http://localhost:4000/users').expectStatus(200);
+    return request(app.getHttpServer()).get('/users').expect(200);
   });
 });
 
 describe('Posts', () => {
   it('should return a list of posts ', () => {
-    return pactum.spec().get('http://localhost:4000/posts').expectStatus(200);
+    return request(app.getHttpServer()).get('/posts').expect(200);
   });
 });
 
