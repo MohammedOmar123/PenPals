3f0e224ff5947afe1c61dd70dae52ed439707cef
Bas-sheikh <basilelshakhe@gmail.com>
2023-03-10 18:25:38 +0200

    handled remove feedback functionality. relates #36

--- server/src/feedback/feedback.service.ts
+++ server/src/feedback/feedback.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/sequelize';
 
 import { CreateFeedbackDto, UpdateFeedbackDto } from './dto';
 import { Feedback } from './entities/feedback.entity';
-import { CREATE_FEEDBACK, UPDATE } from '../core/constant';
+import { CREATE_FEEDBACK, DELETE_FEEDBACK, UPDATE } from '../core/constant';
 
 @Injectable()
 export class FeedbackService {
@@ -18,10 +18,6 @@ export class FeedbackService {
 
   findAll() {
     return this.feedbackRepository.findAll();
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} feedback`;
   }
 
   async update(
@@ -43,7 +39,21 @@ export class FeedbackService {
     else return { message: UPDATE };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} feedback`;
+  async remove(id: number, user: { userId: number; role: string }) {
+    let isDelete = {};
+
+    if (user.role === 'admin') {
+      isDelete = await this.feedbackRepository.destroy({ where: { id } });
+    } else {
+      isDelete = await this.feedbackRepository.destroy({
+        where: { id, userId: user.userId },
+      });
+    }
+
+    if (!isDelete) {
+      throw new NotFoundException("feedback id doesn't exist");
+    }
+
+    return { message: DELETE_FEEDBACK };
   }
 }
